493ae2e02e69f7afcd51ff1aeefb53fdcbc09b50
Nicolas264859 <emailnumber264859@gmail.com>
2023-02-06 15:05:10 +0100

    Add Fade in for email verification

--- src/pages/EmailVerificationRequiredPage.tsx
+++ src/pages/EmailVerificationRequiredPage.tsx
@@ -4,6 +4,7 @@ import { useSelector } from "react-redux";
 import { getUser } from "../selectors/user";
 import { INDEX_URL, LOGIN_URL } from "../constants/urls";
 import { Navigate } from "react-router-dom";
+import { Fade } from "@mui/material";
 
 const EmailVerificationRequiredPage = () => {
   const user = useSelector(getUser);
@@ -15,11 +16,15 @@ const EmailVerificationRequiredPage = () => {
   return (
     <>
       <PageTitle title={"Email Verification Required"} />
-      <PageDescription>
-        Your email address is currently unverified. Please check your email inbox and spam
-        folder for a verification link. Clicking the link will confirm your email address
-        and allow you to access all features of the site.
-      </PageDescription>
+      <Fade in={true}>
+        <div className={"pb-96"}>
+          <PageDescription className={"py-10"}>
+            Your email address is currently unverified. Please check your email inbox and
+            spam folder for a verification link. Clicking the link will confirm your email
+            address and allow you to access all features of the site.
+          </PageDescription>
+        </div>
+      </Fade>
     </>
   );
 };
