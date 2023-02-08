2e8c7a1f811c2a9611f9b2f8aad2509199a6e601
Nicolas264859 <emailnumber264859@gmail.com>
2023-02-08 16:51:26 +0100

    Prettiffy configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   corePlugins: {
     preflight: false,
   },
-  important: '#root',
+  important: "#root",
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
